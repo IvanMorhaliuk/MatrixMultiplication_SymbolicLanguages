49f47acacc03a80ad4430442c28188be191702e4
Ivan Morhaliuk <ivan.morhaliuk@gmail.com>
2021-05-29 15:57:53 +0300

    GUI rewrited and finished

--- src/GUI/gui.py
+++ src/GUI/gui.py
@@ -1,170 +1,202 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 
-def changeSpin1():
-    cols = int(m1HandSpinParam[0].get())
-    rows = int(m1HandSpinParam[1].get())
-    for i in handMatrix1:
-        for j in i:
-            j.grid_remove()
-    handMatrix1.clear()
-    for i in range(rows):
-        handMatrix1.append([ ttk.Entry(handMatrix1Frame,width=7) for j in range(cols)])
-    for i in range(rows):
-        for j in range(cols):
-            handMatrix1[i][j].grid(row=i,column=j)
-def changeMode(event):
-    if mode.get() == "default":
-        handMatrix1Label.place_forget()
-        handMatrix2Label.place_forget()
-        handMatrix1Frame.place_forget()
-        matrixesNotebook.place(relheight=1.0,relwidth=1.0)
+
+
+class GUI:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Matrix Multiplication")
+        self.root.geometry('800x600')
+        self.root.minsize(width=700,height=550)
+
+        self.body = ttk.Frame(self.root)
+        self.matrixesFrame = ttk.Frame(self.body)
+        self.logsFrame = ttk.Frame(self.body)
+        self.configsFrame = ttk.Frame(self.body)
+        self.createMatrixesFrameElems()
+        self.createLogsFrameElems()
+        self.createConfigFrameElems()
+        self.styles()
+    
+    def changeSpin(self,handSpinParam,handMatrix,handMatrixFrame):
+        cols = int(handSpinParam[0].get())
+        rows = int(handSpinParam[1].get())
+        for i in handMatrix:
+            for j in i:
+                j.grid_remove()
+        handMatrix.clear()
+        for i in range(rows):
+            handMatrix.append([ ttk.Entry(handMatrixFrame,width=7) for j in range(cols)])
+        for i in range(rows):
+            for j in range(cols):
+                handMatrix[i][j].grid(row=i,column=j)
+    
+    def changeMode(self,event):
+        if self.mode.get() == "default":
+            self.handMatrix1Label.place_forget()
+            self.handMatrix2Label.place_forget()
+            self.handMatrix3Label.place_forget()
+            self.handMatrix1Frame.place_forget()
+            self.handMatrix2Frame.place_forget()
+            self.handMatrix3Frame.place_forget()
+            self.matrixesNotebook.place(relheight=1.0,relwidth=1.0)
+            for i in range(2):
+                self.m1HandSpinParam[i].grid_remove()
+                self.m2HandSpinParam[i].grid_remove()
+            for i in range(2):
+                self.m1SpinParam[i].grid()
+                self.m2SpinParam[i].grid()
+            for i in range(2):
+                self.m1SpinRange[i].grid()
+                self.m2SpinRange[i].grid()
+                self.m1RangeLabels[i].grid()
+                self.m2RangeLabels[i].grid()
+        else:
+            cols1 = int(self.m1HandSpinParam[0].get())
+            rows1 = int(self.m1HandSpinParam[1].get())
+            cols2 = int(self.m2HandSpinParam[0].get())
+            rows2 = int(self.m2HandSpinParam[1].get())
+            for i in range(2):
+                self.m1SpinRange[i].grid_remove()
+                self.m2SpinRange[i].grid_remove()
+                self.m1RangeLabels[i].grid_remove()
+                self.m2RangeLabels[i].grid_remove()
+                
+
+            self.matrixesNotebook.place_forget()
+            for i in range(2):
+                self.m1SpinParam[i].grid_remove()
+                self.m2SpinParam[i].grid_remove()
+            for i in range(2):
+                self.m1HandSpinParam[i].grid(column=i,row=1)
+                self.m2HandSpinParam[i].grid(column=i,row=1)
+            self.handMatrix1Label.place(x=0,y=0)
+            self.handMatrix1Frame.place(x=0,rely=0.05)
+            for i in range(rows1):
+                for j in range(cols1):
+                    self.handMatrix1[i][j].grid(row=i,column=j)
+            self.handMatrix2Label.place(x=0,rely=0.25)
+            self.handMatrix2Frame.place(x=0,rely=0.30)
+            for i in range(rows2):
+                for j in range(cols2):
+                    self.handMatrix2[i][j].grid(row=i,column=j)
+            self.handMatrix3Label.place(x=0,rely=0.50)
+            self.handMatrix3Frame.place(x=0,rely=0.57,relwidth=1.0)
+            self.handMatrix3.pack(anchor="w",fill="both")
+
+    def createMatrixesFrameElems(self):
+        self.matrixesNotebook = ttk.Notebook(self.matrixesFrame)
+
+        self.matrixes = [ ttk.Frame(self.matrixesNotebook) for i in range(3)]
         for i in range(2):
-            m1HandSpinParam[i].grid_remove()
-            m2HandSpinParam[i].grid_remove()
-        for i in range(2):
-            m1SpinParam[i].grid()
-            m2SpinParam[i].grid()
-    else:
-        matrixesNotebook.place_forget()
-        for i in range(2):
-            m1SpinParam[i].grid_remove()
-            m2SpinParam[i].grid_remove()
-        for i in range(2):
-            m1HandSpinParam[i].grid(column=i,row=1)
-            m2HandSpinParam[i].grid(column=i,row=1)
-        handMatrix1Label.place(x=0,y=0)
-        handMatrix1Frame.place(x=0,rely=0.05)
+            self.matrixesNotebook.add(self.matrixes[i],text='matrix'+str(i+1))
+        self.matrixesNotebook.add(self.matrixes[2],text='result matrix')
+
+        self.matrixesTexts = [ tk.Text(self.matrixes[i],wrap="none",state='disabled') for i in range(3)]
+
+        self.matrixesXscrolls = [ ttk.Scrollbar(self.matrixes[i],orient="horizontal",command=self.matrixesTexts[i].xview) for i in range(3)]
+        self.matrixesYscrolls = [ ttk.Scrollbar(self.matrixes[i],orient="vertical",command=self.matrixesTexts[i].yview) for i in range(3)]
+
         for i in range(3):
-            for j in range(3):
-                handMatrix1[i][j].grid(row=i,column=j)
-        handMatrix2Label.place(x=0,rely=0.2)
-root = tk.Tk()
-root.title("Matrix Multiplication")
-root.geometry('800x600')
-root.minsize(width=500,height=550)
+            self.matrixesTexts[i]['yscrollcommand'] = self.matrixesYscrolls[i].set
+            self.matrixesTexts[i]['xscrollcommand'] = self.matrixesXscrolls[i].set
+
+        self.handMatrix1Label = ttk.Label(self.matrixesFrame,text="Matrix1")
+        self.handMatrix1Frame = ttk.Frame(self.matrixesFrame)
+        self.handMatrix1 = []
+        for i in range(3):
+                self.handMatrix1.append([ ttk.Entry(self.handMatrix1Frame,width=7) for j in range(3)])
+
+        self.handMatrix2Label = ttk.Label(self.matrixesFrame,text="Matrix2")
+        self.handMatrix2Frame = ttk.Frame(self.matrixesFrame)
+        self.handMatrix2 = []
+        for i in range(3):
+                self.handMatrix2.append([ ttk.Entry(self.handMatrix2Frame,width=7) for j in range(3)])
+
+        self.handMatrix3Frame = ttk.Frame(self.matrixesFrame)
+        self.handMatrix3Label = ttk.Label(self.matrixesFrame,text="result")
+        self.handMatrix3 = tk.Text(self.handMatrix3Frame,wrap="none",state='disabled')
+
+    def createLogsFrameElems(self):
+        self.text = tk.Text(self.logsFrame,height=7,state='disabled')
+
+    def createConfigFrameElems(self):
+        self.panes = ttk.PanedWindow(self.configsFrame)
+
+        self.panesFrames = [ ttk.LabelFrame(self.panes,text="matrix " + str(i+1),width=100,height=100) for i in range(2)]
+
+        self.m1ParamLabels = [ttk.Label(self.panesFrames[0],text="Cols"),ttk.Label(self.panesFrames[0],text="Rows")]
+        self.m1RangeLabels = [ttk.Label(self.panesFrames[0],text="From:"),ttk.Label(self.panesFrames[0],text="To:")]
+        self.m1SpinParam = [ttk.Spinbox(self.panesFrames[0],from_=1,to=2000,width=4) for i in range(2)]
+        self.m1HandSpinParamVal = [tk.StringVar(),tk.StringVar()]
+        self.m1HandSpinParam = [ttk.Spinbox(self.panesFrames[0],from_=1,to=3,state='readonly',width=4,textvariable=self.m1HandSpinParamVal[i],command=lambda : self.changeSpin(self.m1HandSpinParam,self.handMatrix1,self.handMatrix1Frame)) for i in range(2)]
+        self.m1SpinRange = [ttk.Spinbox(self.panesFrames[0],from_=1,to=2000,width=4) for i in range(2)]
+
+        for i in self.m1HandSpinParamVal:
+            i.set("3")
+
+        self.m2ParamLabels = [ttk.Label(self.panesFrames[1],text="Cols"),ttk.Label(self.panesFrames[1],text="Rows")]
+        self.m2RangeLabels = [ttk.Label(self.panesFrames[1],text="From:"),ttk.Label(self.panesFrames[1],text="To:")]
+        self.m2SpinParam = [ttk.Spinbox(self.panesFrames[1],from_=1,to=2000,width=4) for i in range(2)]
+        self.m2HandSpinParamVal = [tk.StringVar(),tk.StringVar()]
+        self.m2HandSpinParam = [ttk.Spinbox(self.panesFrames[1],from_=1,to=3,state='readonly',width=4,textvariable=self.m2HandSpinParamVal[i],command=lambda : self.changeSpin(self.m2HandSpinParam,self.handMatrix2,self.handMatrix2Frame)) for i in range(2)]
+        self.m2SpinRange = [ttk.Spinbox(self.panesFrames[1],from_=1,to=2000,width=4) for i in range(2)]
+
+        for i in self.m2HandSpinParamVal:
+            i.set("3")
+
+        for i in range(2):
+            self.panes.add(self.panesFrames[i])
+
+
+        self.typeLabel = ttk.Label(self.configsFrame,text='Type:')
+        self.types = ttk.Combobox(self.configsFrame,values=('integer','double'),state='readonly')
+        self.types.current(0)
+
+        self.modeLabel = ttk.Label(self.configsFrame,text='Mode:')
+        self.mode = ttk.Combobox(self.configsFrame,values=('default','hand'),state='readonly')
+        self.mode.current(0)
+        self.mode.bind("<<ComboboxSelected>>",self.changeMode)
+
+
+        self.button = ttk.Button(self.configsFrame,text="Run Calculations")
+
+    def styles(self):
+        for i in (self.body,self.matrixesFrame,self.logsFrame):
+            i['padding'] = 15
+        self.configsFrame['padding'] = 20
+
+    def render(self):
+        self.body.place(relheight=1.0,relwidth=1.0)
+
+        self.matrixesFrame.place(relheight=0.8,relwidth=0.8)
+        self.matrixesNotebook.place(relheight=1.0,relwidth=1.0)
+        for i in self.matrixesTexts:
+            i.place(relheight=1.0,relwidth=1.0)
 
 
 
+        self.logsFrame.place(rely=0.8,relheight=0.2,relwidth=0.8)
+        self.text.pack(anchor="w",fill="both")
+
+        self.configsFrame.place(relx=0.8,relheight=1.0,relwidth=0.2)
+        for pl,sp,rl,sr in (self.m1ParamLabels,self.m1SpinParam,self.m1RangeLabels,self.m1SpinRange),(self.m2ParamLabels,self.m2SpinParam,self.m2RangeLabels,self.m2SpinRange):
+            for i in range(2):
+                pl[i].grid(column=i,row=0)
+            for i in range(2):
+                sp[i].grid(column=i,row=1)
+            for i in range(2):
+                rl[i].grid(column=i,row=2)
+            for i in range(2):
+                sr[i].grid(column=i,row=3)
 
 
-#MAIN FRAMES
-body = ttk.Frame(root,style='bgLime.TFrame')
-matrixesFrame = ttk.Frame(body,style='bgRed.TFrame')
-logsFrame = ttk.Frame(body,style='bgBlue.TFrame')
-configsFrame = ttk.Frame(body,style='bgGreen.TFrame')
+        self.panes.pack()
+        self.modeLabel.pack(pady="5")
+        self.mode.pack(pady="5")
+        self.typeLabel.pack(pady="5")
+        self.types.pack(pady="5")
+        self.button.pack(pady="5")
 
-
-#MATRIXESFRAME ELEMENTS
-matrixesNotebook = ttk.Notebook(matrixesFrame)
-
-matrixes = [ ttk.Frame(matrixesNotebook) for i in range(3)]
-for i in range(2):
-    matrixesNotebook.add(matrixes[i],text='matrix'+str(i+1))
-matrixesNotebook.add(matrixes[2],text='result matrix')
-
-matrixesTexts = [ tk.Text(matrixes[i],wrap="none",state='disabled') for i in range(3)]
-
-matrixesXscrolls = [ ttk.Scrollbar(matrixes[i],orient="horizontal",command=matrixesTexts[i].xview) for i in range(3)]
-matrixesYscrolls = [ ttk.Scrollbar(matrixes[i],orient="vertical",command=matrixesTexts[i].yview) for i in range(3)]
-
-for i in range(3):
-    matrixesTexts[i]['yscrollcommand'] = matrixesYscrolls[i].set
-    matrixesTexts[i]['xscrollcommand'] = matrixesXscrolls[i].set
-
-handMatrix1Label = ttk.Label(matrixesFrame,text="Matrix1")
-handMatrix1Frame = ttk.Frame(matrixesFrame)
-handMatrix1 = []
-for i in range(3):
-        handMatrix1.append([ ttk.Entry(handMatrix1Frame,width=7) for j in range(3)])
-
-handMatrix2Label = ttk.Label(matrixesFrame,text="Matrix2")
-handMatrix2Frame = ttk.Frame(matrixesFrame)
-handMatrix2 = []
-handMatrix3Label = ttk.Label(matrixesFrame,text="result")
-handMatrix3 = tk.Text(matrixes[i],wrap="none",state='disabled')
-
-#logsFrame elements
-text = tk.Text(logsFrame,height=7,state='disabled')
-
-#CONFIGFRAME ELEMENTS
-panes = ttk.PanedWindow(configsFrame)
-
-panesFrames = [ ttk.LabelFrame(panes,text="matrix " + str(i+1),width=100,height=100) for i in range(2)]
-
-m1ParamLabels = [ttk.Label(panesFrames[0],text="Cols"),ttk.Label(panesFrames[0],text="Rows")]
-m1RangeLabels = [ttk.Label(panesFrames[0],text="From:"),ttk.Label(panesFrames[0],text="To:")]
-m1SpinParam = [ttk.Spinbox(panesFrames[0],from_=1,to=2000,width=4) for i in range(2)]
-m1HandSpinParamVal = [tk.StringVar(),tk.StringVar()]
-m1HandSpinParam = [ttk.Spinbox(panesFrames[0],from_=1,to=3,state='readonly',width=4,textvariable=m1HandSpinParamVal[i],command=changeSpin1) for i in range(2)]
-m1SpinRange = [ttk.Spinbox(panesFrames[0],from_=1,to=2000,width=4) for i in range(2)]
-
-for i in m1HandSpinParamVal:
-    i.set("3")
-
-m2ParamLabels = [ttk.Label(panesFrames[1],text="Cols"),ttk.Label(panesFrames[1],text="Rows")]
-m2RangeLabels = [ttk.Label(panesFrames[1],text="From:"),ttk.Label(panesFrames[1],text="To:")]
-m2SpinParam = [ttk.Spinbox(panesFrames[1],from_=1,to=2000,width=4) for i in range(2)]
-m2HandSpinParam = [ttk.Spinbox(panesFrames[1],from_=1,to=3,state='readonly',width=4) for i in range(2)]
-m2SpinRange = [ttk.Spinbox(panesFrames[1],from_=1,to=2000,width=4) for i in range(2)]
-
-for i in range(2):
-    panes.add(panesFrames[i])
-
-
-typeLabel = ttk.Label(configsFrame,text='Type:')
-types = ttk.Combobox(configsFrame,values=('integer','double'),state='readonly')
-types.current(0)
-
-modeLabel = ttk.Label(configsFrame,text='Mode:')
-mode = ttk.Combobox(configsFrame,values=('default','hand'),state='readonly')
-mode.current(0)
-mode.bind("<<ComboboxSelected>>",changeMode)
-
-
-button = ttk.Button(configsFrame,text="Run Calculations")
-
-
-
-#Styles
-for i in (body,matrixesFrame,logsFrame):
-    i['padding'] = 15
-configsFrame['padding'] = 20
-ttk.Style().configure('bgRed.TFrame',background='red')
-ttk.Style().configure('bgBlue.TFrame',background='blue')
-ttk.Style().configure('bgLime.TFrame',background='lime')
-ttk.Style().configure('bgGreen.TFrame',background='green')
-
-
-
-
-
-#grid
-
-body.place(relheight=1.0,relwidth=1.0)
-
-matrixesFrame.place(relheight=0.8,relwidth=0.7)
-matrixesNotebook.place(relheight=1.0,relwidth=1.0)
-for i in matrixesTexts:
-    i.place(relheight=1.0,relwidth=1.0)
-
-
-
-logsFrame.place(rely=0.8,relheight=0.2,relwidth=0.8)
-
-configsFrame.place(relx=0.7,relheight=1.0,relwidth=0.3)
-for pl,sp,rl,sr in (m1ParamLabels,m1SpinParam,m1RangeLabels,m1SpinRange),(m2ParamLabels,m2SpinParam,m2RangeLabels,m2SpinRange):
-    for i in range(2):
-        pl[i].grid(column=i,row=0)
-    for i in range(2):
-        sp[i].grid(column=i,row=1)
-    for i in range(2):
-        rl[i].grid(column=i,row=2)
-    for i in range(2):
-        sr[i].grid(column=i,row=3)
-
-
-panes.pack()
-mode.pack()
-
-root.mainloop()
+        self.root.mainloop()
--- src/main.py
+++ src/main.py
@@ -1,45 +1,6 @@
-import tkinter as tk
-import tkinter.ttk as ttk
+from GUI.gui import *
 
-""" def onChange():
-    for i in entrys:
-        i.grid_forget()
-    entrys.clear()
-    for i in range(int(spin.get())):
-        entrys.append(ttk.Entry(root,width=4))
-    for i in range(int(spin.get())):
-        entrys[i].grid() """
-    
+mainWindow = GUI()
 
-root = tk.Tk()
-root.title("Matrix Multiplication")
-root.geometry('900x600')
-root.resizable(False,False)
+mainWindow.render()
 
-body = ttk.Frame(root,style='bgRed.TFrame')
-matrixesFrame = ttk.Frame(body)
-logsFrame = ttk.Frame(body)
-configsFrame = ttk.Frame(body)
-
-#
-
-#STYLES
-for i in (body,matrixesFrame,logsFrame):
-    i['padding'] = 15
-configsFrame['padding'] = 20
-
-ttk.Style().configure('bgRed.TFrame',background='red')
-
-
-""" spin = ttk.Spinbox(root,from_=1,to=3,values=("1","2","3"),state='readonly',width=4,command=onChange)
-spin.grid()
-entrys = []   """
-
-#GRID
-for i in (root,body,matrixesFrame,logsFrame):
-    i.columnconfigure(1, weight=1)
-    i.rowconfigure(1, weight=1)
-body.grid(column=0,row=0,columnspan=2,rowspan=2,sticky="nsew")
-for i in range(3):
-    ttk.Label(body,text="zhopa").grid(row=0,column=i,sticky="w")
-root.mainloop()
